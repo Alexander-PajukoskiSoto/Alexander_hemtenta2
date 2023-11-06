4336e82e4392afa49f2c297d5c78cbf5dbbfba41
Alexander Pajukoski Soto <alexander.pajukoskisoto@elev.ga.ntig.se>
2023-11-06 09:59:09 +0100

    check connect plus variable and creattable

--- index.php
+++ index.php
@@ -3,11 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>crud_app</title>
 </head>
 <body>
     <?php
-        echo "hi";
+    //define servername user password and name of database
+    $serverName = "localhost";
+    $dbmsUser = "root";
+    $dbmsPassword = "";
+    $dbName = "crud_app";
+
+    $dbms_connect = mysqli_conntect($serverName,$dbmsUser,$dbmsPassword,$dbName);
+
+    $sqlCreateTableProduct = "CREATE TABLE product(productID INT PRIMARY KEY, productName VARCHAR,)"
+    //connected
+    if (!$dbms_connect){
+        echo "hlep";
+    }
     ?>
 </body>
 </html>

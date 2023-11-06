a3ff466f281c1be8a457ccfcf7480d2285bb253f
Alexander Pajukoski Soto <alexander.pajukoskisoto@elev.ga.ntig.se>
2023-11-06 10:03:07 +0100

    commetn

--- index.php
+++ index.php
@@ -13,13 +13,17 @@
     $dbmsPassword = "";
     $dbName = "crud_app";
 
+    // connect
     $dbms_connect = mysqli_conntect($serverName,$dbmsUser,$dbmsPassword,$dbName);
 
+    // variable that creates a table
     $sqlCreateTableProduct = "CREATE TABLE product(productID INT PRIMARY KEY, productName VARCHAR, price INT, productImage IMAGE)";
     //connected
     if (!$dbms_connect){
         echo "hlep";
     }
+
+    
     ?>
 </body>
 </html>

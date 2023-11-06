56ecf7d78742877f7ddabd9bef89a3cc66fecfd6
Alexander Pajukoski Soto <alexander.pajukoskisoto@elev.ga.ntig.se>
2023-11-06 21:08:23 +0100

    insert sample data(through console)
    :construction_worker:

--- funktioner/connect.php
+++ funktioner/connect.php
@@ -35,15 +35,40 @@ $productConnect = mysqli_connect($serverName,$user,$password,$db);
 if ($productConnect->connect_error) {
     die("Connection failed: " . $productConnect->connect_error);
   }
-
+//create table
 $productTable = "CREATE TABLE productInfo(
     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     productName VARCHAR(30) NOT NULL,
-    price INT(9) NOT NULL,
+    productDescription text NOT NULL,
+    price double NOT NULL,
+    created datetime NOT NULL,
+    modified timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 )";
-if ($productConnect->query($productTable) === TRUE) {
-    echo "Table MyGuests created successfully";
-  } else {
-    echo "Error creating table: " . $productConnect->error;
-  }
-echo"hi";
+//creation check
+//WORKS
+// if ($productConnect->query($productTable) === TRUE) {
+//     echo "Table productInfo created successfully";
+//   } else {
+//     echo "Error creating table: " . $productConnect->error;
+//   }
+  
+//sample data
+
+  // $sampleData="INSERT INTO productInfo (id, productName, productDescription) VALUES
+  //  ('1', 'idk', 'idkDescription')";
+  // mysqli_query($productConnect,$sampleData);
+  ////////////////////////////////////////////////////
+  //Gave up and added through console in phpMyAdmin//
+  //////////////////////////////////////////////////
+
+
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //INSERT INTO `productInfo` (`id`, `productName`, `productDescription`, `price`, `created`, `modified`) VALUES
+  //(1, 'Basketball', 'A ball used in the NBA.', 49.99, '2015-08-02 12:04:03', '2015-08-06 06:59:18'),
+  //(3, 'Gatorade', 'This is a very good drink for athletes.', 1.99, '2015-08-02 12:14:29', '2015-08-06 06:59:18'),
+  //(4, 'Eye Glasses', 'It will make you read better.', 6, '2015-08-02 12:15:04', '2015-08-06 06:59:18'),
+  //(5, 'Trash Can', 'It will help you maintain cleanliness.', 3.95, '2015-08-02 12:16:08', '2015-08-06 06:59:18'),
+  // (6, 'Mouse', 'Very useful if you love your computer.', 11.35, '2015-08-02 12:17:58', '2015-08-06 06:59:18'),
+  // (7, 'Earphone', 'You need this one if you love music.', 7, '2015-08-02 12:18:21', '2015-08-06 06:59:18'),
+  // (8, 'Pillow', 'Sleeping well is important.', 8.99, '2015-08-02 12:18:56', '2015-08-06 06:59:18');
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////

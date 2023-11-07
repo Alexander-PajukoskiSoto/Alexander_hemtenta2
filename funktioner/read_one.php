4268d27cd82426384793875fe4df2e9f994f92dc
Alexander Pajukoski Soto <alexander.pajukoskisoto@elev.ga.ntig.se>
2023-11-07 12:12:43 +0100

    read_one done

--- funktioner/read_one.php
+++ funktioner/read_one.php
@@ -3,19 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Read</title>
+    <title>Document</title>
 </head>
 <body>
-<?php
+       <!-- container -->
+       <div class="container">
+        <div class="page-header">
+            <h1>Read Product</h1>
+        </div>
+        <?php
 // get passed parameter value, in this case, the record ID
 // isset() is a PHP function used to verify if a value is there or not
 $id=isset($_GET['id']) ? $_GET['id'] : die('ERROR: Record ID not found.');
 //include database connection
-include 'config/database.php';
+include './connect.php';
 // read current record's data
 try {
     // prepare select query
-    $query = "SELECT id, productName, productDescription, price FROM products WHERE id = ? LIMIT 0,1";
+    $query = "SELECT id, productName, productDescription, price FROM productInfo WHERE id = ? LIMIT 0,1";
     $stmt = $con->prepare( $query );
     // this is the first question mark
     $stmt->bindParam(1, $id);
@@ -24,8 +29,8 @@ try {
     // store retrieved row to a variable
     $row = $stmt->fetch(PDO::FETCH_ASSOC);
     // values to fill up our form
-    $productName = $row['name'];
-    $productDescription = $row['description'];
+    $productName = $row['productName'];
+    $productDescription = $row['productDescription'];
     $price = $row['price'];
 }
 // show error
@@ -33,5 +38,27 @@ catch(PDOException $exception){
     die('ERROR: ' . $exception->getMessage());
 }
 ?>
+        <!--we have our html table here where the record will be displayed-->
+<table class='table table-hover table-responsive table-bordered'>
+    <tr>
+        <td>Name</td>
+        <td><?php echo htmlspecialchars($productName, ENT_QUOTES);  ?></td>
+    </tr>
+    <tr>
+        <td>Description</td>
+        <td><?php echo htmlspecialchars($productDescription, ENT_QUOTES);  ?></td>
+    </tr>
+    <tr>
+        <td>Price</td>
+        <td><?php echo htmlspecialchars($price, ENT_QUOTES);  ?></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td>
+            <a href='../index.php' class='btn btn-danger'>Back to read products</a>
+        </td>
+    </tr>
+</table>
+    </div> <!-- end .container -->
 </body>
 </html>

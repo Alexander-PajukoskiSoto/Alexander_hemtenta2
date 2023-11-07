d50bfbfe8a424692ffb8a32f15bd5cd937e6e4ec
Alexander Pajukoski Soto <alexander.pajukoskisoto@elev.ga.ntig.se>
2023-11-07 09:59:29 +0100

    Remade connection and create works

---
+++ funktioner/create.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    include './connect.php';
+if($_POST){
+    // include database connection
+    try{
+        // insert query
+        $query = "INSERT INTO productInfo SET productName=:productName, productDescription=:productDescription, price=:price, created=:created";
+        // prepare query for execution
+        $stmt = $con->prepare($query);
+        // posted values
+        $name=htmlspecialchars(strip_tags($_POST['productName']));
+        $description=htmlspecialchars(strip_tags($_POST['productDescription']));
+        $price=htmlspecialchars(strip_tags($_POST['price']));
+        // bind the parameters
+        $stmt->bindParam(':productName', $name);
+        $stmt->bindParam(':productDescription', $description);
+        $stmt->bindParam(':price', $price);
+        // specify when this record was inserted to the database
+        $created=date('Y-m-d H:i:s');
+        $stmt->bindParam(':created', $created);
+        // Execute the query
+        if($stmt->execute()){
+            echo "<div>Record was saved.</div>";
+        }else{
+            echo "<div>Unable to save record.</div>";
+        }
+    }
+    // show error
+    catch(PDOException $exception){
+        die('ERROR: ' . $exception->getMessage());
+    }
+}
+?>
+<!-- html form here where the product information will be entered -->
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" enctype="multipart/form-data">
+    <table>
+        <tr>
+            <td>Name</td>
+            <td><input type='text' name='productName'/></td>
+        </tr>
+        <tr>
+            <td>Description</td>
+            <td><textarea name='productDescription'></textarea></td>
+        </tr>
+        <tr>
+            <td>Price</td>
+            <td><input type='text' name='price'/></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>
+                <input type='submit' value='Save' />
+                <a href='index.php'>Back to read products</a>
+            </td>
+        </tr>
+    </table>
+</form>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Products!</title>
 </head>
 <body>
     <?php include "./funktioner/connect.php"?>
+    <?php include "./funktioner/create.php"?>
 </body>
 </html>
